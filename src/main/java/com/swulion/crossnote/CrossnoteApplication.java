cda36e8a5d07c34569700c162035dd6404e89692
Yang-Boyun <boyunyang@naver.com>
2025-10-23 23:16:30 +0900

    init: upload project to GitHub

---
+++ src/main/java/com/swulion/crossnote/CrossnoteApplication.java
@@ -0,0 +1,13 @@
+package com.swulion.crossnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swulion/crossnote/CrossnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swulion.crossnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
