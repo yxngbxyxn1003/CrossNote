d0190d42c0ab6fd3a8619e3e5f9d6c817484be83
5y0ung4 <qwerrr504@naver.com>
2025-11-14 17:29:44 +0900

    chore: 테스트 내용 원복

--- src/main/java/com/swulion/crossnote/config/SecurityConfig.java
+++ src/main/java/com/swulion/crossnote/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import com.swulion.crossnote.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
